4bcdb23d5b5904a6b22687fb476c58d74bb1a70a
Teh Hung Wei <122148337+juxue97@users.noreply.github.com>
2025-02-12 15:52:02 +0800

    docs: removed auth and some common package to https://github.com/juxue97/GolangBackend

--- auth/cmd/migrate/migrations/000002_add_indexes.up.sql
+++
@@ -1,3 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-
-CREATE INDEX IF NOT EXISTS idx_users_username ON users (username);
--- auth/internal/utils.go
+++
@@ -1,5 +0,0 @@
-package internal
-
-import "github.com/go-playground/validator/v10"
-
-var Validate *validator.Validate = validator.New(validator.WithRequiredStructEnabled())
